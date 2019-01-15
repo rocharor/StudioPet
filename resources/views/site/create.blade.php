c9518be3b4bacefdf0c51d79297553afc6c707e1
Ricardo Rocha <rocharor@gmail.com>
2019-01-15 12:09:36 -0200

    Campos form required

--- resources/views/site/create.blade.php
+++ resources/views/site/create.blade.php
@@ -24,9 +24,9 @@
         <br>
         <form action="/store" method="post">
             {{ csrf_field() }}
-            <label>Nome:</label> <input type="text" name='name' class='form-control'>
-            <label>Nome Animal:</label> <input type="text" name='name_animal' class='form-control'>
-            <label>Telefone:</label> <input type="text" name='phone' class='form-control'>
+            <label>Nome:</label> <input type="text" name='name' class='form-control' required>
+            <label>Nome Animal:</label> <input type="text" name='name_animal' class='form-control' required>
+            <label>Telefone:</label> <input type="text" name='phone' class='form-control' required>
             <label>Observações:</label> <br><textarea name="obs" rows="10" class='form-control'></textarea>
             <br>
             <input type="submit" value="Salvar" class='btn btn-success'>
